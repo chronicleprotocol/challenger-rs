33f762d256aefc2e078e9b5813db22ce884dbd1a
Konstantin Zolotarev <konstantin.zolotarev@gmail.com>
2023-10-09 21:48:58 +0300

    Added to_block

--- src/challenger.rs
+++ src/challenger.rs
@@ -18,6 +18,7 @@ use ethers::{
     contract::{abigen, Contract, LogMeta},
     core::types::{Address, ValueOrArray, U64},
     providers::Middleware,
+    types::BlockNumber,
 };
 use eyre::Result;
 use log::{debug, error, info};
